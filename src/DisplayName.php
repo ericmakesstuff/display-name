bb4abdd0efb773f648e5064afba60fd10ad37e8c
Eric Blount <eblount@gmail.com>
2015-05-17 21:38:57 +0000

    Complete functionality of main DisplayName class

--- src/DisplayName.php
+++ src/DisplayName.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DisplayName;
+namespace EricMakesStuff\DisplayName;
 
 class DisplayName
 {
@@ -12,8 +12,86 @@ class DisplayName
         // constructor body
     }
 
-    public function initials($name)
+    public function show($name, $method = 'firstNameLastInitial')
+    {
+        if (method_exists($this, $method)) {
+            return $this->{$method}($name);
+        }
+
+        return '';
+    }
+
+    public function fullName($name)
     {
         return $name;
+    }
+
+    public function firstName($name)
+    {
+        $parsed_name = $this->parseName($name);
+
+        return array_shift($parsed_name);
+    }
+
+    public function firstNameLastInitial($name)
+    {
+        $parsed_name = $this->parseName($name);
+
+        $name = array_shift($parsed_name);
+
+        if (count($parsed_name)) {
+            $name .= ' ' . strtoupper($parsed_name[(count($parsed_name) - 1)][0]);
+        }
+
+        return $name;
+    }
+
+    public function firstNameLastInitialWithPeriod($name)
+    {
+        $parsed_name = $this->parseName($name);
+
+        $name = array_shift($parsed_name);
+
+        if (count($parsed_name)) {
+            $name .= ' ' . strtoupper($parsed_name[(count($parsed_name) - 1)][0]) . '.';
+        }
+
+        return $name;
+    }
+
+    public function initials($name)
+    {
+        return implode('', $this->getInitialsArray($name));
+    }
+
+    public function initialsWithPeriods($name)
+    {
+        if (empty($name))
+            return '';
+
+        return implode('.', $this->getInitialsArray($name)) . '.';
+    }
+
+    public function initialsWithSpaces($name)
+    {
+        return implode(' ', $this->getInitialsArray($name));
+    }
+
+    protected function getInitialsArray($name)
+    {
+        $initials = array();
+
+        foreach ($this->parseName($name) as $word) {
+            if (!empty($word)) {
+                $initials[] = strtoupper($word[0]);
+            }
+        }
+
+        return $initials;
+    }
+
+    protected function parseName($name)
+    {
+        return explode(' ', trim($name));
     }
 }
---
+++ tests/DisplayNameTest.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace EricMakesStuff\DisplayName\Test;
+
+use EricMakesStuff\DisplayName\DisplayName;
+
+class DisplayNameTest extends \PHPUnit_Framework_TestCase
+{
+    protected $displayName;
+
+    public function setUp()
+    {
+        $this->displayName = new DisplayName;
+    }
+    /**
+     * @dataProvider names
+     */
+    public function testFullName($name)
+    {
+        $this->assertEquals($this->displayName->fullName($name), $name);
+    }
+
+    /**
+     * @dataProvider names
+     */
+    public function testFirstName($name, $variants)
+    {
+        $this->assertEquals($this->displayName->firstName($name), $variants['firstName']);
+    }
+
+    /**
+     * @dataProvider names
+     */
+    public function testFirstNameLastInitial($name, $variants)
+    {
+        $this->assertEquals($this->displayName->firstNameLastInitial($name), $variants['firstNameLastInitial']);
+    }
+
+    /**
+     * @dataProvider names
+     */
+    public function testFirstNameLastInitialWithPeriod($name, $variants)
+    {
+        $this->assertEquals(
+            $this->displayName->firstNameLastInitialWithPeriod($name),
+            $variants['firstNameLastInitialWithPeriod']
+        );
+    }
+
+    /**
+     * @dataProvider names
+     */
+    public function testInitials($name, $variants)
+    {
+        $this->assertEquals($this->displayName->initials($name), $variants['initials']);
+    }
+
+    /**
+     * @dataProvider names
+     */
+    public function testInitialsWithPeriods($name, $variants)
+    {
+        $this->assertEquals($this->displayName->initialsWithPeriods($name), $variants['initialsWithPeriods']);
+    }
+
+    /**
+     * @dataProvider names
+     */
+    public function testInitialsWithSpaces($name, $variants)
+    {
+        $this->assertEquals($this->displayName->initialsWithSpaces($name), $variants['initialsWithSpaces']);
+    }
+
+    /**
+     * @dataProvider names
+     */
+    public function testShow($name, $variants)
+    {
+        // With no arguments, show the first name and last initial
+        $this->assertEquals($this->displayName->show($name), $variants['firstNameLastInitial']);
+
+        $this->assertEquals($this->displayName->show($name, 'fullName'), $name);
+
+        // We should be able to call $displayName->show('John Smith', 'firstName') and get 'John'
+        // The same goes for every function
+        foreach ($variants as $method => $result) {
+            $this->assertEquals($this->displayName->show($name, $method), $result);
+        }
+    }
+
+    public function names()
+    {
+        return array(
+            array("John Smith", array(
+                'firstName' => 'John',
+                'initials' => 'JS',
+                'initialsWithPeriods' => 'J.S.',
+                'initialsWithSpaces' => 'J S',
+                'firstNameLastInitial' => 'John S',
+                'firstNameLastInitialWithPeriod' => 'John S.',
+            )),
+            array("Bob R. Johnson", array(
+                'firstName' => 'Bob',
+                'initials' => 'BRJ',
+                'initialsWithPeriods' => 'B.R.J.',
+                'initialsWithSpaces' => 'B R J',
+                'firstNameLastInitial' => 'Bob J',
+                'firstNameLastInitialWithPeriod' => 'Bob J.',
+            )),
+            array("Sally Moore Stevenson", array(
+                'firstName' => 'Sally',
+                'initials' => 'SMS',
+                'initialsWithPeriods' => 'S.M.S.',
+                'initialsWithSpaces' => 'S M S',
+                'firstNameLastInitial' => 'Sally S',
+                'firstNameLastInitialWithPeriod' => 'Sally S.',
+            )),
+            array("Leo deMarco", array(
+                'firstName' => 'Leo',
+                'initials' => 'LD',
+                'initialsWithPeriods' => 'L.D.',
+                'initialsWithSpaces' => 'L D',
+                'firstNameLastInitial' => 'Leo D',
+                'firstNameLastInitialWithPeriod' => 'Leo D.',
+            )),
+            array("Jack", array(
+                'firstName' => 'Jack',
+                'initials' => 'J',
+                'initialsWithPeriods' => 'J.',
+                'initialsWithSpaces' => 'J',
+                'firstNameLastInitial' => 'Jack',
+                'firstNameLastInitialWithPeriod' => 'Jack',
+            )),
+            array("", array(
+                'firstName' => '',
+                'initials' => '',
+                'initialsWithPeriods' => '',
+                'initialsWithSpaces' => '',
+                'firstNameLastInitial' => '',
+                'firstNameLastInitialWithPeriod' => '',
+            )),
+        );
+    }
+}
