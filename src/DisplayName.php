978f3299727b20965d05e53e877fc02e65ea5acb
Eric Blount <eblount@gmail.com>
2015-05-17 20:45:08 +0000

    Change name of main class

---
+++ src/DisplayName.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace DisplayName;
+
+class DisplayName
+{
+    /**
+     * Create a new DisplayName Instance
+     */
+    public function __construct()
+    {
+        // constructor body
+    }
+
+    public function initials($name)
+    {
+        return $name;
+    }
+}
